e8e272c0de63da82b60081c6e6f31e14c6f5daae
slytechroot <slytechrootadmin@protonmail.com>
2022-01-23 22:28:45 -0800

    Week 3

---
+++ Week3/Labs/Lab3Ex15.py
@@ -0,0 +1,7 @@
+'''
+1.	Given prod = {'tomato': 'red', 'squash': 'yellow', 'potato': 'brown', 'avocado': 'green'}. 
+Complete the following tasks:
+A.	Use a function to display each key and its value. Example: ‘tomato’: ‘red’
+B.	Include globals() and locals() to display container details.
+'''
+prod = {'tomato': 'red', 'squash': 'yellow', 'potato': 'brown', 'avocado': 'green'}
